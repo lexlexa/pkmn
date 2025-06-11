c85493ab84a8d29864d9386b14b0b12ec1e3a43f
lexlexa <lexlexa.new@gmail.com>
2025-06-11 16:22:37 +0300

    fix storage

--- pokemon-helper-api/files/constants.js
+++ pokemon-helper-api/files/constants.js
@@ -1,33 +1,14 @@
 import path from "path";
+import { fileURLToPath } from "url";
 
-export const FIRST_FILE_PATH = path.join(
-  process.cwd(),
-  "..",
-  "storage",
-  "first.csv"
-);
-export const SECOND_FILE_PATH = path.join(
-  process.cwd(),
-  "..",
-  "storage",
-  "second.csv"
-);
-export const NEED_CARDS_PATH = path.join(
-  process.cwd(),
-  "..",
-  "storage",
-  "need.csv"
-);
-export const PRICES_CARDS_PATH = path.join(
-  process.cwd(),
-  "..",
-  "storage",
-  "prices.csv"
-);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-export const SALE_CARDS_PATH = path.join(
-  process.cwd(),
-  "..",
-  "storage",
-  "sale.json"
-);
+const storagePath = path.join(__dirname, "..", "..", "storage");
+
+export const FIRST_FILE_PATH = path.join(storagePath, "first.csv");
+export const SECOND_FILE_PATH = path.join(storagePath, "second.csv");
+export const NEED_CARDS_PATH = path.join(pstoragePath, "need.csv");
+export const PRICES_CARDS_PATH = path.join(storagePath, "prices.csv");
+
+export const SALE_CARDS_PATH = path.join(storagePath, "sale.json");
