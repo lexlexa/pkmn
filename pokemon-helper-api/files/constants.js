8240afd8ee561a7c97ac505f32909b46ed7e67e2
lexlexa <lexlexa.new@gmail.com>
2025-06-11 16:22:54 +0300

    fix

--- pokemon-helper-api/files/constants.js
+++ pokemon-helper-api/files/constants.js
@@ -8,7 +8,7 @@ const storagePath = path.join(__dirname, "..", "..", "storage");
 
 export const FIRST_FILE_PATH = path.join(storagePath, "first.csv");
 export const SECOND_FILE_PATH = path.join(storagePath, "second.csv");
-export const NEED_CARDS_PATH = path.join(pstoragePath, "need.csv");
+export const NEED_CARDS_PATH = path.join(storagePath, "need.csv");
 export const PRICES_CARDS_PATH = path.join(storagePath, "prices.csv");
 
 export const SALE_CARDS_PATH = path.join(storagePath, "sale.json");
