13ad7554691dff2c5a7457e82427cdb9d166054a
lexlexa <lexlexa.new@gmail.com>
2025-06-16 11:18:07 +0300

    banners

--- pokemon-helper/vite.config.ts
+++ pokemon-helper/vite.config.ts
@@ -7,6 +7,11 @@ export default defineConfig({
   build: {
     outDir: "../pokemon-helper-api/public",
     emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: `assets/[name].[ext]`,
+      },
+    },
   },
   server: {
     proxy: {
