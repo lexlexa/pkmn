f463b4be7bf746751011266eb2b4f5239cb1c389
lexlexa <lexlexa.new@gmail.com>
2025-07-21 19:53:39 +0300

    filter

--- pokemon-helper/src/pages/SaleRare/components/RareCardsList/RareCardsList.tsx
+++ pokemon-helper/src/pages/SaleRare/components/RareCardsList/RareCardsList.tsx
@@ -14,10 +14,11 @@ import {
   EyeOutlined,
 } from "@ant-design/icons";
 import { useState } from "react";
-import { Button } from "antd";
+import { Button, Select } from "antd";
 
 export const RareCardsList = () => {
   const list = useUnit($rareSaleCards);
+  const [filter, setFilter] = useState<"all" | "no_prices" | "hidden">("all");
   const [expanded, setExpanded] = useState<string[]>([]);
 
   const handleHide = (expansion: TRareSaleCards) => {
@@ -36,12 +37,71 @@ export const RareCardsList = () => {
     }
   };
 
+  const options = [
+    { label: "Все", value: "all" },
+    // { label: "Без цен", value: "no_prices" },
+    { label: "Скрытые", value: "hidden" },
+  ];
+
+  const handleChange = (value: "all" | "no_prices" | "hidden") => {
+    setFilter(value);
+  };
+
+  const filteredList = list.reduce((acc, curr) => {
+    if (filter === "hidden") {
+      if (curr.isHidden) return [...acc, curr];
+      const cards = curr.cards.filter((card) => card.isHidden);
+      if (cards.length > 0) {
+        return [...acc, { ...curr, cards }];
+      }
+      return acc;
+    }
+
+    if (filter === "no_prices") {
+      const cards = curr.cards.filter((card) => !card.price);
+      if (cards.length > 0) {
+        return [...acc, { ...curr, cards }];
+      }
+      return acc;
+    }
+    return [...acc, curr];
+  }, [] as TRareSaleCards[]);
+  // const filteredList = list
+  //   .filter((item) => {
+  //     if (filter === "hidden") return item.isHidden;
+  //     return true;
+  //   })
+  //   .map((item) => ({
+  //     ...item,
+  //     cards: item.cards.filter((card) => {
+  //       if (filter === "hidden") return card.isHidden;
+  //       if (filter === "no_prices") return !card.price;
+  //       return true;
+  //     }),
+  //   }));
   return (
     <div className={styles.container}>
       <div className={styles.panel}>
-        <Button onClick={() => saveSaleRare()}>Сохранить</Button>
+        <div>
+          <Button
+            color="cyan"
+            variant="outlined"
+            style={{ width: "100%" }}
+            onClick={() => saveSaleRare()}
+          >
+            Сохранить
+          </Button>
+        </div>
+        <div>
+          <Select
+            style={{ width: "100%" }}
+            value={filter}
+            options={options}
+            onChange={handleChange}
+          />
+        </div>
       </div>
-      {list.map((exp) => (
+      {filteredList.map((exp) => (
         <div key={exp.expansion} className={styles.expansion}>
           <div className={styles.title}>
             <div className={styles.titleText}>
