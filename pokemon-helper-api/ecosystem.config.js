79cc6e2483f5051b4311ac38f4c50395bcb93505
lexlexa <lexlexa.new@gmail.com>
2025-10-16 17:34:34 +0300

    f

--- pokemon-helper-api/ecosystem.config.js
+++ pokemon-helper-api/ecosystem.config.js
@@ -6,7 +6,11 @@ export default {
       interpreter: "node",
       interpreter_args: "--import=tsx", // для tsx
       instances: 1,
+      cwd: "/var/www/pkmn/pokemon-helper-api",
       exec_mode: "fork",
+      error_file: "./logs/error.log",
+      out_file: "./logs/output.log",
+      log_date_format: "YYYY-MM-DD HH:mm:ss",
       env: {
         NODE_ENV: "development",
       },
